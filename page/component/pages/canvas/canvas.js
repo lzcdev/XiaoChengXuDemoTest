7f61af9d48172c3810e7a472589583a41767ce97
“鲁志超” <zhichao.lu@trustway.cn>
2018-02-07 15:14:14 +0800

    add login and getUserInfo

--- page/component/pages/canvas/canvas.js
+++ page/component/pages/canvas/canvas.js
@@ -1,66 +1,58 @@
-// page/component/canvas/canvas.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-  
-  },
+    this.position = {
+      x: 150,
+      y: 150,
+      vx: 2,
+      vy: 2
+    }
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+    this.drawBall()
+    this.interval = setInterval(this.drawBall, 17)
   },
+  drawBall: function () {
+    var p = this.position
+    p.x += p.vx
+    p.y += p.vy
+    if (p.x >= 300) {
+      p.vx = -2
+    }
+    if (p.x <= 7) {
+      p.vx = 2
+    }
+    if (p.y >= 300) {
+      p.vy = -2
+    }
+    if (p.y <= 7) {
+      p.vy = 2
+    }
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+    var context = wx.createContext()
+
+    function ball(x, y) {
+      context.beginPath(0)
+      context.arc(x, y, 5, 0, Math.PI * 2)
+      context.setFillStyle('#1aad19')
+      context.setStrokeStyle('rgba(1,1,1,0)')
+      context.fill()
+      context.stroke()
+    }
+
+    ball(p.x, 150)
+    ball(150, p.y)
+    ball(300 - p.x, 150)
+    ball(150, 300 - p.y)
+    ball(p.x, p.y)
+    ball(300 - p.x, 300 - p.y)
+    ball(p.x, 300 - p.y)
+    ball(300 - p.x, p.y)
+
+    wx.drawCanvas({
+      canvasId: 'canvas',
+      actions: context.getActions()
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    clearInterval(this.interval)
   }
 })
